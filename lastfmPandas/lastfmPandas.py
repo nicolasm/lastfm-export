a3c1721535fd5bcbc75e2b11a8a36f1c19d565f7
Nicolas Meier <me@nclsmr.com>
2020-05-04 22:48:35 +0200

    Add play by months over years

--- lastfmPandas/lastfmPandas.py
+++ lastfmPandas/lastfmPandas.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import colorlover as cl
 import pandas
 
 query_top = """
@@ -21,68 +20,49 @@ from top t
 
 queries_top_for_function = {}
 queries_top_for_function['Artist'] = query_top + \
-                        """
-                        union all
-                        select 'Remaining {entity}s' as {entity}_name,
-                        ((select tc.play_count from total_count tc)
-                        -
-                        (select sum(play_count) from top)) as play_count
+                                     """
+                                     union all
+                                     select 'Remaining {entity}s' as {entity}_name,
+                                     ((select tc.play_count from total_count tc)
+                                     -
+                                     (select sum(play_count) from top)) as play_count
 
-                        """
+                                     """
 
 queries_top_for_function['Album'] = query_top + \
-                        """
-                        union all
-                        select 'Remaining {entity}s' as {entity}_name,
-                        '...' as album_name,
-                        ((select tc.play_count from total_count tc)
-                        -
-                        (select sum(play_count) from top)) as play_count
+                                    """
+                                    union all
+                                    select 'Remaining {entity}s' as {entity}_name,
+                                    '...' as album_name,
+                                    ((select tc.play_count from total_count tc)
+                                    -
+                                    (select sum(play_count) from top)) as play_count
 
-                        """
+                                    """
 
 queries_top_for_function['Track'] = query_top + \
-                       """
-                       union all
-                       select 'Remaining {entity}s' as {entity}_name,
-                       '...' as artist_name,
-                       '...' as album_name,
-                       ((select tc.play_count from total_count tc)
-                       -
-                       (select sum(play_count) from top)) as play_count
+                                    """
+                                    union all
+                                    select 'Remaining {entity}s' as {entity}_name,
+                                    '...' as artist_name,
+                                    '...' as album_name,
+                                    ((select tc.play_count from total_count tc)
+                                    -
+                                    (select sum(play_count) from top)) as play_count
 
-                       """
+                                    """
 
-query_top_for_year = """
-with top as (
-    select v.*
-    from (select @for_year := {for_year} p) parm, view_top_{entity}s_for_year v
-    limit {limit}
-),
-total_count as (
-    select v.play_count
-    from view_play_count v
-)
-
-select t.*
-from top t
-"""
-
-
-
-query_top_for_year = 'select v.* from (select @for_year:=%s p) parm, view_top_%s_for_year v limit %s'
 query_total_count = 'select v.* from (select @nb_days:=%s p) parm, view_play_count v'
 query_total_count_for_year = 'select v.* from (select @for_year:=%s p) parm, view_play_count_for_year v'
 
-colors = cl.scales['10']['qual']['Set3']
 
-
-def retrieve_play_count_by_month_as_dataframe(cursor):
-    cursor.execute('select * from view_play_count_by_month')
+def retrieve_play_count_by_month_as_dataframe(cursor, year):
+    cursor.execute('select * from view_play_count_by_month v where substr(v.month, 1, 4) = %s' % year)
     rows = cursor.fetchall()
     df = pandas.DataFrame([[ij for ij in i] for i in rows])
     df.rename(columns={0: 'Month', 1: 'PlayCount'}, inplace=True)
-    df = df.sort_values(by='Month', ascending=[0])
+    df['Month'] = pandas.to_datetime(df['Month'], format='%Y-%m')
+    df = df.sort_values(by='Month')
     return df
 
 
@@ -112,80 +92,91 @@ def retrieve_recent_plays_as_dataframe(cursor):
 
 
 def retrieve_total_play_count(cursor, nb_days):
-    cursor.execute(query_total_count % (nb_days))
+    cursor.execute(query_total_count % nb_days)
     return cursor.fetchone()[0]
 
 
 def retrieve_total_play_count_for_year(cursor, for_year):
-    cursor.execute(query_total_count % (for_year))
+    cursor.execute(query_total_count % for_year)
     return cursor.fetchone()[0]
 
 
 def retrieve_top_artists_as_dataframe(cursor, nb_days, limit):
     cursor.execute(
-        queries_top_for_function['Artist'].format(function_name='for_nb_days', entity='artist', function_value=nb_days,
+        queries_top_for_function['Artist'].format(function_name='for_nb_days',
+                                                  entity='artist',
+                                                  function_value=nb_days,
                                                   limit=limit))
-    rows = cursor.fetchall()
-
-    df = pandas.DataFrame(rows, columns=['Artist', 'PlayCount'], dtype='int64')
-    df = df.sort_values(by='PlayCount', ascending=[1])
-    return df
+    return create_artists_dataframe(cursor)
 
 
 def retrieve_top_artists_for_year_as_dataframe(cursor, for_year, limit):
     cursor.execute(
-        queries_top_for_function['Artist'].format(function_name='for_year', entity='artist', function_value=for_year,
+        queries_top_for_function['Artist'].format(function_name='for_year',
+                                                  entity='artist',
+                                                  function_value=for_year,
                                                   limit=limit))
-    rows = cursor.fetchall()
+    return create_artists_dataframe(cursor)
 
+
+def create_artists_dataframe(cursor):
+    rows = cursor.fetchall()
     df = pandas.DataFrame(rows, columns=['Artist', 'PlayCount'], dtype='int64')
-    df = df.sort_values(by='PlayCount', ascending=[1])
+    df = df.sort_values(by='PlayCount', ascending=False)
     return df
 
 
 def retrieve_top_albums_as_dataframe(cursor, nb_days, limit):
-    cursor.execute(queries_top_for_function['Album'].format(function_name='for_nb_days', entity='album', function_value=nb_days,
-                                                            limit=limit))
-    rows = cursor.fetchall()
-
-    df = pandas.DataFrame(rows, columns=['Album', 'Artist', 'PlayCount'], dtype='int64')
-    df["ArtistAlbum"] = df["Artist"] + " - " + df["Album"]
-    df = df.sort_values(by='PlayCount', ascending=[0])
-    return df
+    cursor.execute(
+        queries_top_for_function['Album'].format(function_name='for_nb_days',
+                                                 entity='album',
+                                                 function_value=nb_days,
+                                                 limit=limit))
+    return create_albums_dataframe(cursor)
 
 
 def retrieve_top_albums_for_year_as_dataframe(cursor, for_year, limit):
     cursor.execute(
-        queries_top_for_function['Album'].format(function_name='for_year', entity='album', function_value=for_year,
+        queries_top_for_function['Album'].format(function_name='for_year',
+                                                 entity='album',
+                                                 function_value=for_year,
                                                  limit=limit))
-    rows = cursor.fetchall()
+    return create_albums_dataframe(cursor)
 
-    df = pandas.DataFrame(rows, columns=['Album', 'Artist', 'PlayCount'], dtype='int64')
+
+def create_albums_dataframe(cursor):
+    rows = cursor.fetchall()
+    df = pandas.DataFrame(rows, columns=['Album', 'Artist', 'PlayCount'],
+                          dtype='int64')
     df["ArtistAlbum"] = df["Artist"] + " - " + df["Album"]
-    df = df.sort_values(by='PlayCount', ascending=[1])
+    df = df.sort_values(by='PlayCount', ascending=False)
     return df
 
 
 def retrieve_top_tracks_as_dataframe(cursor, nb_days, limit):
-    cursor.execute(queries_top_for_function['Track'].format(function_name='for_nb_days', entity='track', function_value=nb_days,
-                                                            limit=limit))
-    rows = cursor.fetchall()
-
-    df = pandas.DataFrame(rows, columns=['Track', 'Artist', 'Album', 'PlayCount'], dtype='int64')
-    df["ArtistAlbumTrack"] = df["Artist"] + " - " + df["Album"] + " - " + df[
-        "Track"]
-    df = df.sort_values(by='PlayCount', ascending=[0]);
-    return df
+    cursor.execute(
+        queries_top_for_function['Track'].format(function_name='for_nb_days',
+                                                 entity='track',
+                                                 function_value=nb_days,
+                                                 limit=limit))
+    return create_tracks_dataframe(cursor)
 
 
 def retrieve_top_tracks_for_year_as_dataframe(cursor, for_year, limit):
-    cursor.execute(query_top_for_year % (for_year, 'tracks', limit))
-    rows = cursor.fetchall()
+    cursor.execute(
+        queries_top_for_function['Track'].format(function_name='for_year',
+                                                 entity='track',
+                                                 function_value=for_year,
+                                                 limit=limit))
+    return create_tracks_dataframe(cursor)
 
-    df = pandas.DataFrame([[ij for ij in i] for i in rows])
-    df.rename(columns={0: 'Track', 1: 'Artist', 2: 'Album', 3: 'PlayCount'},
-              inplace=True);
+
+def create_tracks_dataframe(cursor):
+    rows = cursor.fetchall()
+    df = pandas.DataFrame(rows,
+                          columns=['Track', 'Artist', 'Album', 'PlayCount'],
+                          dtype='int64')
     df["ArtistAlbumTrack"] = df["Artist"] + " - " + df["Album"] + " - " + df[
         "Track"]
-    df = df.sort_values(by='PlayCount', ascending=[0]);
+    df = df.sort_values(by='PlayCount', ascending=False);
     return df
--- plot-artists-tops.py
+++ plot-artists-tops.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from math import ceil
 
 import MySQLdb
 import colorlover as cl
@@ -12,7 +13,9 @@ import netrcfile
 from lastfmPandas.lastfmPandas import retrieve_top_artists_as_dataframe, \
     retrieve_top_albums_as_dataframe, \
     retrieve_top_artists_for_year_as_dataframe, \
-    retrieve_top_albums_for_year_as_dataframe
+    retrieve_top_albums_for_year_as_dataframe, \
+    retrieve_top_tracks_for_year_as_dataframe, retrieve_top_tracks_as_dataframe, \
+    retrieve_play_count_by_month_as_dataframe
 
 login = netrcfile.retrieve_from_netrc('lastfm.mysql')
 
@@ -20,7 +23,7 @@ mysql = MySQLdb.connect(
     user=login[0], passwd=login[2], db=login[1], charset='utf8')
 mysql_cursor = mysql.cursor()
 
-colors = cl.to_numeric(cl.scales['10']['qual']['Set3'])
+colors = cl.to_numeric(cl.scales['12']['qual']['Set3'])
 
 # df = retrieve_play_count_by_month_as_dataframe(mysql_cursor)
 # df.head()
@@ -44,7 +47,6 @@ def plot_pie(df, legend_column):
 def plot_barh(df, xcolumn):
     ax = df.plot.barh(x=xcolumn, y='PlayCount', figsize=(20, 5), rot=0,
                       color=selected_colors)
-    # ax.legend()
 
     # create a list to collect the plt.patches data
     totals = []
@@ -60,9 +62,9 @@ def plot_barh(df, xcolumn):
 
 
 def plot_tops_durations():
-    nbDays = ['7', '30', '90', '120', '180', '365', 'null']
+    nb_days = ['7', '30', '90', '120', '180', '365', 'null']
 
-    for duration in nbDays:
+    for duration in nb_days:
         suffix = 'overall'
         folder = suffix
         if duration != 'null':
@@ -71,70 +73,105 @@ def plot_tops_durations():
 
         Path('./tops/durations/%s' % folder).mkdir(parents=True, exist_ok=True)
 
-        df = retrieve_top_artists_as_dataframe(mysql_cursor, duration, 10)
+        df = retrieve_top_artists_as_dataframe(mysql_cursor, duration, 20)
         df = df[df.Artist != 'Remaining artists']
         plot_pie(df, 'Artist')
         plt.savefig('tops/durations/%s/top-artists-%s-pie.png' % (folder, suffix),
                     format='png', dpi=150)
         plt.close()
 
-        df = retrieve_top_artists_as_dataframe(mysql_cursor, duration, 20)
-        df = df[df.Artist != 'Remaining artists']
         plot_barh(df, 'Artist')
         plt.savefig('tops/durations/%s/top-artists-%s-barh.png' % (folder, suffix),
                     format='png', dpi=150)
         plt.close()
 
-        df = retrieve_top_albums_as_dataframe(mysql_cursor, duration, 10)
+        df = retrieve_top_albums_as_dataframe(mysql_cursor, duration, 20)
         df = df[df.Album != 'Remaining albums']
         plot_pie(df, 'ArtistAlbum')
         plt.savefig('tops/durations/%s/top-albums-%s-pie.png' % (folder, suffix),
                     format='png', dpi=150)
         plt.close()
 
-        df = retrieve_top_albums_as_dataframe(mysql_cursor, duration, 20)
-        df = df[df.Album != 'Remaining albums']
         plot_barh(df, 'Album')
         plt.savefig('tops/durations/%s/top-albums-%s-barh.png' % (folder, suffix),
                     format='png', dpi=150)
         plt.close()
 
+        df = retrieve_top_tracks_as_dataframe(mysql_cursor, duration, 20)
+        df = df[df.Track != 'Remaining tracks']
+        plot_pie(df, 'ArtistAlbumTrack')
+        plt.savefig('tops/durations/%s/top-tracks-%s-pie.png' % (folder, suffix),
+                    format='png', dpi=150)
+
 
 def plot_tops_years():
     now = datetime.now()
-    for x in range(2008, now.year + 1):
+    for x in range(2006, now.year + 1):
         year = str(x)
 
         Path('./tops/years/%s' % year).mkdir(parents=True, exist_ok=True)
 
-        df = retrieve_top_artists_for_year_as_dataframe(mysql_cursor, year, 10)
+        df = retrieve_top_artists_for_year_as_dataframe(mysql_cursor, year, 20)
         df = df[df.Artist != 'Remaining artists']
         plot_pie(df, 'Artist')
         plt.savefig('tops/years/{year}/top-artists-{year}-pie.png'.format(year=year),
                     format='png', dpi=150)
         plt.close()
 
-        df = retrieve_top_artists_for_year_as_dataframe(mysql_cursor, year, 20)
-        df = df[df.Artist != 'Remaining artists']
         plot_barh(df, 'Artist')
         plt.savefig('tops/years/{year}/top-artists-{year}-barh.png'.format(year=year),
                     format='png', dpi=150)
         plt.close()
 
-        df = retrieve_top_albums_for_year_as_dataframe(mysql_cursor, year, 10)
+        df = retrieve_top_albums_for_year_as_dataframe(mysql_cursor, year, 20)
         df = df[df.Album != 'Remaining albums']
-        plot_pie(df, 'Album')
+        plot_pie(df, 'ArtistAlbum')
         plt.savefig('tops/years/{year}/top-albums-{year}-pie.png'.format(year=year),
                     format='png', dpi=150)
         plt.close()
 
-        df = retrieve_top_albums_for_year_as_dataframe(mysql_cursor, year, 20)
-        df = df[df.Album != 'Remaining albums']
         plot_barh(df, 'Album')
         plt.savefig('tops/years/{year}/top-albums-{year}-barh.png'.format(year=year),
                     format='png', dpi=150)
         plt.close()
 
+        df = retrieve_top_tracks_for_year_as_dataframe(mysql_cursor, year, 20)
+        df = df[df.Track != 'Remaining tracks']
+        plot_pie(df, 'ArtistAlbumTrack')
+        plt.savefig('tops/years/{year}/top-tracks-{year}-pie.png'.format(year=year),
+                    format='png', dpi=150)
 
-plot_tops_durations()
-# plot_tops_years()
+
+#plot_tops_durations()
+#plot_tops_years()
+
+now = datetime.now()
+years = range(2006, now.year + 1)
+
+print('years', len(years))
+nb_cols = 4
+nb_rows = ceil(len(years) / nb_cols)
+
+print(nb_rows, ' ', nb_cols)
+
+f, ax_array = plt.subplots(nb_rows, nb_cols, figsize=(15,15))
+print(type(ax_array))
+
+year_index = 0
+for i in range(0, nb_rows):
+    for j in range(0, nb_cols):
+        if year_index < len(years):
+            year = str(years[year_index])
+            df = retrieve_play_count_by_month_as_dataframe(mysql_cursor, year)
+
+            ax_array[i, j].plot(df.Month, df.PlayCount)
+            ax_array[i, j].set_title(year)
+            year_index = year_index + 1
+
+        ax_array[i, j].get_xaxis().set_visible(False)
+        # plt.subplots(nb_rows, nb_cols, i + 1, figsize=(15,15))
+        # df.plot(x='Month', y='PlayCount')
+        # plt.plot(df.Month, df.PlayCount)
+
+plt.savefig('tops/play-count-by-month.png', format='png', dpi=150)
+plt.close()
