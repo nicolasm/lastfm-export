09a7ba32706844e50e12efedc933cf872e635b7c
Nicolas Meier <me@nclsmr.com>
2020-05-14 16:34:23 +0200

    Add runConfig for cron

--- runExport.sh
+++ runExport.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -
 cd ~/Code/GitHub/lastfm-export/
+if [ -r "./runConfig" ]; then
+  . "./runConfig"
+fi
+
 ./venv/bin/python export.py
 if [[ $? = 0 ]]; then
     ./venv/bin/python import.py
