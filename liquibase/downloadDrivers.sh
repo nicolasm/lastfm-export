ed0a4e55dceb36c7cb7d72840ff0197d6f81fb8d
Nicolas Meier <me@nclsmr.com>
2020-05-17 15:29:20 +0200

    Add download drivers script

---
+++ liquibase/downloadDrivers.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash -
+curl -O https://bitbucket.org/xerial/sqlite-jdbc/downloads/sqlite-jdbc-3.30.1.jar
+curl -O https://downloads.mariadb.com/Connectors/java/connector-java-2.6.0/mariadb-java-client-2.6.0.jar
