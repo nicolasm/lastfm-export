f1a586a2fb51e7ef2c8687661fa1afbb1e325170
Nicolas Meier <nicolasm@users.noreply.github.com>
2022-02-13 21:59:17 +0100

    Delete installModules.sh

--- installModules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -
-./venv/bin/pip install pyyaml
-./venv/bin/pip install mysqlclient
-./venv/bin/pip install requests
-./venv/bin/pip install pillow
-./venv/bin/pip install pandas
-./venv/bin/pip install matplotlib
-./venv/bin/pip install colorlover
-./venv/bin/pip install python-telegram-bot
