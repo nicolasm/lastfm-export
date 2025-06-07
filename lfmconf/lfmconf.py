f5780e646ce95e577597e6419d546e0cb1e18a92
Nicolas Meier <nicolas@nclsmr.com>
2025-06-07 14:44:13 +0200

    Define fallback value when getting XDG_CONFIG_HOME env variable

--- lfmconf/lfmconf.py
+++ lfmconf/lfmconf.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 
-XDG_CONFIG_HOME = os.environ['XDG_CONFIG_HOME']
+XDG_CONFIG_HOME = os.environ.get('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
 
 with open('%s/lastfm-export/lastfm-export.yml' % XDG_CONFIG_HOME,
           'r') as stream:
